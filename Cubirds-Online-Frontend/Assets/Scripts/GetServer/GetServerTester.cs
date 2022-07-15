63169cf251a55e9cbc51ec56edcc7100aa81738e
Mr.ture.China <mr.true.china@gmail.com>
2022-07-15 23:44:19 +0800

    添加获取服务器的测试代码。 添加一个全局的函数式调用的信息面板。

---
+++ Cubirds-Online-Frontend/Assets/Scripts/GetServer/GetServerTester.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GetServerTester : MonoBehaviour
+{
+    /// <summary>
+    /// 获取服务器配置列表的 URL
+    /// </summary>
+    private const string GET_SERVER_CONFIG_URL = "https://gitcode.net/M_t_C/Get-Config-From-Github-Development-And-Test/-/raw/main/Config/CO-Test-Config.json";
+
+    /// <summary>
+    /// 服务器配置列表
+    /// </summary>
+    public static List<Dictionary<string, string>> ServerConfigs { get; set; } = new List<Dictionary<string, string>>();
+
+    private void Start()
+    {
+        StartCoroutine(GetServerConfig(GET_SERVER_CONFIG_URL));
+    }
+
+    /// <summary>
+    /// 获取服务器配置列表的协程
+    /// </summary>
+    /// <param name="uri"></param>
+    /// <returns></returns>
+    private IEnumerator GetServerConfig(string uri)
+    {
+        Debug.Log("开始获取服务器信息");
+
+        InfoDialogController.Instance.Show("获取服务器信息");
+
+        // 准备一个字典树组来接收配置信息
+        List<Dictionary<string, string>> serverConfigList;
+
+        // 使用 using 来自动解决请求释放的问题
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // 发出请求并等待响应
+            yield return webRequest.SendWebRequest();
+
+            // 如果发生连接错误
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.LogError("获取服务器配置列表时发生连接错误: " + webRequest.error);
+
+                // 直接结束协程
+                yield break;
+            }
+
+            // 把获取到的 JSON 字符串转为字典
+            serverConfigList = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(webRequest.downloadHandler.text);
+        }
+
+        Debug.Log("获取到服务器配置列表：");
+        serverConfigList.ForEach(config => {
+            Debug.Log(config["name"] + " | " + config["gameServer"] + " | " + config["hotUpdateServer"]);
+        });
+
+        InfoDialogController.Instance.Close();
+
+        // 保存到服务器配置列表里
+        ServerConfigs = serverConfigList;
+    }
+}
---
+++ Cubirds-Online-Frontend/Assets/Scripts/Global UI/Info Dialog/InfoDialogController.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins;
+
+/// <summary>
+/// 显示信息的对话框的控制器
+/// </summary>
+public class InfoDialogController : MonoBehaviour
+{
+    #region 单例部分
+    /// <summary>
+    /// 实例
+    /// </summary>
+    public static InfoDialogController Instance
+    {
+        get
+        {
+            if(instance != null)
+            {
+                return instance;
+            }
+
+            lock (typeof(InfoDialogController))
+            {
+                if(instance == null)
+                {
+                    // 从 Resources 加载预制
+                    GameObject prefab = Resources.Load<GameObject>("Info Dialog");
+
+                    // 实例化并保存组件
+                    instance = Instantiate(prefab).GetComponent<InfoDialogController>();
+
+                    // 禁用实例，默认不显示
+                    instance.gameObject.SetActive(false);
+                }
+
+                return instance;
+            }
+        }
+    }
+    private static InfoDialogController instance;
+    #endregion
+
+    [SerializeField]
+    private RectTransform rectTransform;
+
+    /// <summary>
+    /// 显示信息，在指定时间后关闭
+    /// </summary>
+    /// <param name="info"></param>
+    /// <param name="displayTime">显示信息的时间（秒）</param>
+    public void Show(string info, float displayTime)
+    {
+        // 显示信息
+        Show(info);
+
+        // 用协程延时关闭
+        StartCoroutine(DelayClose(displayTime));
+    }
+
+    /// <summary>
+    /// 延时关闭面板
+    /// </summary>
+    /// <param name="delayTime"></param>
+    /// <returns></returns>
+    private IEnumerator DelayClose(float delayTime)
+    {
+        // 等待时间
+        yield return new WaitForSeconds(delayTime);
+
+        // 关闭面板
+        Close();
+    }
+
+    /// <summary>
+    /// 显示信息，不会自动关闭，只有调用 <see cref="Close"/> 才会关闭
+    /// </summary>
+    /// <param name="info"></param>
+    public void Show(string info)
+    {
+        // 激活实例
+        gameObject.SetActive(true);
+
+        // 首先把缩放调到 0，在视觉上隐藏起来
+        rectTransform.localScale = new Vector3(1, 0, 1);
+
+        // 渐变显示出来
+        rectTransform.DOScaleY(1, 0.1f);
+    }
+
+    /// <summary>
+    /// 关闭面板
+    /// </summary>
+    public void Close()
+    {
+        // 渐变关闭
+        rectTransform.DOScaleY(0, 0.1f)
+            // 完成后禁用实例
+            .onComplete = () => { gameObject.SetActive(false); };
+    }
+}
